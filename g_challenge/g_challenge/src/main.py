f65545147818a8156c8f2b0da5dd23d56394709b
HectorInove <hector.inove@gmail.com>
2023-08-15 03:31:33 -0300

    seccond point finished

--- g_challenge/g_challenge/src/main.py
+++ g_challenge/g_challenge/src/main.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--createschemas', type=bool, required=False)
 
 description = f'''<a href="https://github.com/HectorInove/g-challenge">
-    <img width=10% src="https://octodex.github.com/images/NUX_Octodex.gif"/> Follow the repo...</a>'''
+    <img width=10% src="https://raw.githubusercontent.com/HectorInove/g-challenge/develop/g_challenge/g_challenge/src/docs/assets/octocat.gif"/> Follow the repository...</a>'''
 
 app = FastAPI(
     title="G-challenge API service docs page",
